7ab74d607e5c9d5209cc6a263d44ed9ca09cb8d9
Sachin Patil <gowthambhat0@gmail.com>
2021-05-20 10:01:30 +0000

    fixed static

--- masterDoYourMath/settings.py
+++ masterDoYourMath/settings.py
@@ -119,4 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/skprs/do-your-math/masterDoYourMath/static'
+STATIC_ROOT = '/home/skprs/masterDoYourMath/static'
